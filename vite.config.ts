b200093cc05bfc8369c60422f93bdad809fdab88
Clara <clara.morin@campus-la-chataigneraie.org>
2025-08-18 18:27:06 +0200

    vute

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/project-neumorphism-2k25/",
+  base: "/portfolio/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
